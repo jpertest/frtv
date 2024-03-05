6b935e1d2e5871d3f136962186995a2e549b35e5
Julien PEREZ <julien.perez@cgi.com>
2024-03-05 15:54:38 +0100

    Added button and label event

--- FranceTV/Form1.cs
+++ FranceTV/Form1.cs
@@ -16,5 +16,15 @@ namespace FranceTV
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Clicked on label");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Clicked on button");
+        }
     }
 }
